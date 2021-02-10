80cfa83dba684593156222b1191396957b2b71cc
zhangxiaolin <zhangxiaolin@sunniwell.net>
2021-02-10 21:45:56 +0800

    修复bug

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,10 +1,10 @@
-type application = {name:string,data:any}
 
-export function savelocal(name:application, data: application) {
-    localStorage.setItem(name,JSON.stringify(data))
+// localStorage
+export function savelocal(name:string, data: any) {
+    window.localStorage.setItem(name,JSON.stringify(data))
 }
 
-export function getlocal(name:application){
+export function getlocal(name:string){
     const result = window.localStorage.getItem(name);
     if (result && result !== 'undefined') {
     return JSON.parse(result);
@@ -12,22 +12,23 @@ export function getlocal(name:application){
     return false;
 }
 
-export function removelocal(name:application){
-    localStorage.removeItem(name)
+export function removelocal(name:string){
+    window.localStorage.removeItem(name)
 }
 
-export function savesession(name:application,data:application){
-    sessionStorage(name,JSON.string(data))
+  //  sessionStorage
+export function savesession(name:string,data:any){
+    window.sessionStorage.setItem(name,JSON.stringify(data))
 }
 
-export function getsession(name:application){
-    const result = window.sessionStorage.getItem(name);
-    if (result && result !== 'undefined') {
-    return JSON.parse(result);
-     }
-    return false;
+export function getsession(name:string){
+   const  result =  window.sessionStorage.getItem(name)
+   if (result && result !== 'undefined'){
+      return JSON.parse(result);
+   }
+      return false
 }
 
-export function removesession(name:application){
-    sessionStorage.removeItem(name)
+export function removesession(name:string){
+    window.sessionStorage.removeItem(name)
 }
