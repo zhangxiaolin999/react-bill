1673136f9777adcbba48a4ddd1a491c8e3d7af74
zhang19980107 <943248247@qq.com>
2021-02-11 13:39:48 +0800

    完成 支出 收入切换

--- src/pages/Money/CategorySection.tsx
+++ src/pages/Money/CategorySection.tsx
@@ -1,5 +1,6 @@
-import styled from "styled-components";
-import React from 'react';
+import
+    styled from "styled-components";
+import React, {useState} from 'react';
 
 const CategorySection = styled.section`
 font-size:24px;
@@ -9,7 +10,7 @@ font-size:24px;
   > li{
     width:50%;
     text-align:center;
-    padding:16px 0px;
+    padding:16px 0;
     position: relative;
     &.selected::after{
       content:'';
@@ -22,16 +23,28 @@ font-size:24px;
     }
   }
 }
-`
-const CategorysSection: React.FC = () => {
+`;
+const CategoriesSection: React.FC = () => {
+    const categoriesMap = {0:'支出',1:'收入'};
+    type Keys  = keyof typeof categoriesMap;
+    const [categoriesList] = useState<Keys[]>([0,1]);
+    const [categories,setCategories] = useState(0);
+
     return (
         <CategorySection>
             <ul>
-                <li className='selected'>支出</li>
-                <li>收入</li>
+                {
+                    categoriesList.map(c =>
+                        <li
+                        className={categories === c ? 'selected':'' }
+                        key={c}
+                        onClick={()=> setCategories(c) }
+                        >{categoriesMap[c]}</li>
+                    )
+                }
             </ul>
         </CategorySection>
     )
 }
 
-export default CategorysSection
+export default CategoriesSection
