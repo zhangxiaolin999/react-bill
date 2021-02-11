b7556aeb7dc3ce3fd0106b37ed283d27328b3657
zhang19980107 <943248247@qq.com>
2021-02-11 11:56:12 +0800

    添加标签ui

--- src/pages/Money/TagsSection.tsx
+++ src/pages/Money/TagsSection.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
-import React from 'react';
+import React, {useState} from 'react';
+import { Modal } from 'antd-mobile';
+const prompt = Modal.prompt;
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
 background-color:#fff;
 padding:12px 16px;
 flex-grow:1;
@@ -18,6 +20,9 @@ align-items:flex-start;
     padding:3px 18px;
     font-size:14px;
     margin:8px 12px;
+    &.selected{
+    background: #6699CC;
+   }
   }
 }
 > button{
@@ -28,19 +33,39 @@ align-items:flex-start;
   color:#666;
   margin-top:8px;
 }
-`
+`;
 
-const TagSection:React.FC = ()=>{
+const TagSection:React.FC = (props)=>{
+    const [tags,setTags] = useState<string[]>(['衣','食','住','行']);
+    const [selectedTags,setSelectedTags] = useState<string[]>([]);
+    const onAddTag = () =>{
+        prompt('新增标签', '', [ { text: '取消' },
+            { text: '提交', onPress: ((value)=>{
+                    const tagName  = value;
+                    if (tagName) setTags([...tags,tagName]);
+                }) },
+        ], 'default', '')
+    };
+    const onToggleTag = (tag:string) =>{
+        const index = selectedTags?.indexOf(tag);
+        if(0 <= index){
+            setSelectedTags(selectedTags?.filter(t => t !== tag))
+        }else{
+            setSelectedTags([...selectedTags,tag])
+        }
+    }
+    const  GetClassName = (tag:string) => selectedTags.indexOf(tag) >= 0 ? 'selected' :''
     return(
-        <TagsSection>
+    <Wrapper>
      <ol>
-       <li>衣</li>
-       <li>食</li>
-       <li>住</li>
-       <li>行</li>
+         {tags.map(tag => <li
+             onClick={()=>onToggleTag(tag)}
+             key={tag}
+             className={GetClassName(tag)}
+         >{tag}</li>)}
      </ol>
-     <button>新增标签</button>
-    </TagsSection>
+     <button onClick={() => onAddTag()}>新增标签</button>
+    </Wrapper>
     )
 }
 
