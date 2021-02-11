124a7b9454c0e6eb603720e265a5bb07985d60a4
zhang19980107 <943248247@qq.com>
2021-02-11 18:05:18 +0800

    封装组件

--- src/pages/Money/index.tsx
+++ src/pages/Money/index.tsx
@@ -3,21 +3,29 @@ import styled from "styled-components";
 import TagsSection from 'pages/Money/TagsSection'
 import NoteSection from 'pages/Money/NotesSection'
 import CategorySection from 'pages/Money/CategorySection'
-import NumberPadSection from 'pages/Money/NumberPadSection'
-import React from 'react';
+import NumberPadSection from 'pages/Money/NumberPadSection/NumberPadSection'
+import React, {useState} from 'react';
 
 const MyLayout  = styled(Layout)`
   display:flex;
   flex-direction:column;
 `;
 
+type Category = 0 | 1
+
 const Money:React.FC = ()=>{
+    const [selected,setSelected] = useState({
+        tags:[] as string[],
+        note: '',
+        category: 0 as Category,
+        amount: 0
+    });
     return(
       <MyLayout>
-        <TagsSection />
-        <NoteSection />
-        <CategorySection />
-        <NumberPadSection />
+        <TagsSection value={selected.tags} onChange={(tags)=>setSelected({...selected, tags })} />
+        <NoteSection value={selected.note} onChange={(note)=>{setSelected({...selected,note})}} />
+        <CategorySection value={selected.category}  onChange={(category)=>{setSelected({...selected,category})}} />
+        <NumberPadSection value={selected.amount}  onChange={(amount)=>{setSelected({...selected,amount})}} />
       </MyLayout>
     )
 };
